302e48d603e6aca41a48e843e33629acf70e4d58
Quint Guvernator <quintus.public@gmail.com>
2014-06-17 11:01:27 -0600

    begin to implement scraping instances from soup

--- agile.py
+++ agile.py
@@ -27,48 +27,64 @@ from bs4 import BeautifulSoup as bs
 from docopt import docopt
 import pathlib
 
-class Layout:
-    def __init__(self, parent, height, width, orientation, gravity=None, subGravity=None):
-        self.height = self.inheritable(height, parent, lambda x: x.height)
-        self.width = self.inheritable(width, parent, lambda x: x.width)
-        self.orientation = orientation
 
-    @staticmethod
-    def inheritable(value, parent, getFn):
-        if value in ("match_parent", "fill_parent"):
-            return getFn(parent)
-        else:
-            return value
+def resource(value, resourcesPath)
+    if not value.startswith("@+"):
+        return value
+
+    value.replace("@+", '', 1)
+    filename, key = tuple(value.split('/', 1))
+    with open(resourcesPath / (filename + ".xml")) as f:
+        rsoup = bs(f).find("resources")
+
+    toReturn = rsoup.find(name=key).string
+
+    return toReturn
+
+
+def inheritable(value, parent, getFn):
+    if value in ("match_parent", "fill_parent"):
+        return getFn(parent)
+    else:
+        return value
+
+
+class Layout:
+
+    def __init__(self, parent, height, width, orientation, gravity=None, subGravity=None, children):
+        self.height = inheritable(height, parent, lambda x: x.height)
+        self.width = inheritable(width, parent, lambda x: x.width)
+        self.orientation = orientation
+        self.children = children
+
+    @classmethod
+    def fromSoup(cls, parent, soup, resourcesPath):
+        id = soup["android:id"]
+        height = soup["android:layout_height"]
+        width = soup["android:layout_width"]
+        # TODO
+
+        new = cls(id, parent, height, width, orientation, gravity, subGravity, children)
+        return new
+
 
 class Clickable:
+
     def __init__(self, id, parent, height, width, text="", gravity=None):
         self.id = id
         self.parent = parent
-        self.height = height
-        self.width = width
+        self.height = inheritable(height, parent, lambda x: x.height)
+        self.width = inheritable(height, parent, lambda x: x.height)
         self.text = text
         self.gravity = gravity
 
-    @staticmethod
-    def resource(value, resourcesPath)
-        if not value.startswith("@+"):
-            return value
-
-        value.replace("@+", '', 1)
-        filename, key = tuple(value.split('/', 1))
-        with open(resourcesPath / (filename + ".xml")) as f:
-            rsoup = bs(f).find("resources")
-
-        toReturn = rsoup.find(name=key).string
-        if toReturn is None:
-            raise NameError("Key not found in resources!")
-
-        return toReturn
-
     @classmethod
-    def fromSoup(cls, parent, csoup, resourcesPath):
-        text = self.resource(soup["android:text"])
-        gravity = self.resource(soup["android:gravity"])
+    def fromSoup(cls, parent, soup, resourcesPath):
+        id = soup["android:id"]
+        height = soup["android:layout_height"]
+        width = soup["android:layout_height"]
+        text = self.resource(soup["android:text"], resourcesPath)
+        gravity = soup["android:gravity"]
 
         new = cls(id, parent, height, width, text, gravity)
         return new
